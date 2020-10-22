c852d1acb03c0722e9f4593241aaebc63b90a1e6
Jonatas Gabriel <72398708+0jedaaa@users.noreply.github.com>
2020-10-22 01:48:55 -0300

    Update file.js

--- javascript/file.js
+++ javascript/file.js
@@ -1,18 +1,4 @@
 
-   /* var capturando = document.getElementById('textbusca').value;
-
-
-    function capturar(){
-        capturando = document.getElementById('textbusca').value;
-
-        if (capturando == "Produtos"){
-            alert("Ok");
-
-        }
-
-    }
-
-    */
    $(window).on('load', function() {
        $('#slider').nivoSlider();
    });
