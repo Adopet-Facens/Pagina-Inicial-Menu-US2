ed16c8885138b49e6299da46524843e996878b3b
Jonatas Gabriel <72398708+0jedaaa@users.noreply.github.com>
2020-10-22 01:41:48 -0300

    Add files via upload

---
+++ javascript/file.js
@@ -0,0 +1,18 @@
+
+   /* var capturando = document.getElementById('textbusca').value;
+
+
+    function capturar(){
+        capturando = document.getElementById('textbusca').value;
+
+        if (capturando == "Produtos"){
+            alert("Ok");
+
+        }
+
+    }
+
+    */
+   $(window).on('load', function() {
+       $('#slider').nivoSlider();
+   });
